38ff8a9b2622fd6a868cc1e5b463a348c877d181
007sair <007sair@163.com>
2015-08-18 18:16:15 +0800

    update

---
+++ static/js/effect.js
@@ -0,0 +1,64 @@
+$(function(){
+
+    var srcTop = $('.anchorList').offset().top;
+
+    $(window).scroll(function() {
+        var curTop = $(window).scrollTop();
+        if (curTop > srcTop) {
+            $('.anchorList').css({
+                'position': 'fixed',
+                'top': '0'
+            });
+        } else {
+            $('.anchorList').css({
+                'position': 'absolute',
+                'top': '2em'
+            });
+        }
+        /*
+        for (var i = 0; i < arrPos.length; i++) {
+            if (curTop > arrPos[i]) {
+                $('.anchorList li').removeClass('active');
+                $('.anchorList a').eq(i).parent().addClass("active");
+            };
+        };
+        */
+
+    });
+
+    $('.lotus-post h3').each(function(index, el) {
+
+        $('.anchorList').children('ul').append('<li><a>' + $(el).html() + '</a></li><li style="list-style-type:none"><ul></ul></li>');
+
+        var $tags = $(this).nextUntil('h3');
+
+        $tags.each(function(i, el) {
+            var $h4 = $(this)[0].tagName.toLowerCase();
+            if ($h4 === 'h4') {
+                $('.anchorList').find('li[style] ul').eq(index).append('<li><a href="">' + $(this).html() + '</a></li>')
+                //console.log($(this), i, index)
+            };
+        });
+        //console.log('---------------');
+    });
+
+    $('.lotus-post').find('h3,h4').each(function(index, el) {
+        $(this).attr({
+            'class': 'anchor',
+            'id': 'anchor' + index
+        });
+    });
+
+    $('.anchorList').find('a').each(function(index, el) {
+        $(this).attr('href', '#anchor' + index);
+    }).on('click', function() {
+        $('.anchorList li').removeClass('active');
+        $(this).parent().addClass("active")
+    });
+
+    var arrPos = [];
+    $('.anchor').each(function(index, el) {
+        arrPos.push($(this).offset().top);
+    });
+
+})
