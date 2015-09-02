faf6962d10d505113173ac6b785aa01fac650009
007sair <007sair@163.com>
2015-09-02 17:31:49 +0800

    update

--- static/js/effect.js
+++ static/js/effect.js
@@ -11,34 +11,7 @@ $(function(){
         }
     });
 
-    if ($('.lotus-post h3').length === 0) {
-        $('.lotus-post h4').each(function(index, el) {
-            $('.block-catalog .conter').children('ul').append('<li><a>' + $(this).text() + '</a></li>');
-        });
-    };
-
-    $('.lotus-post h3').each(function(index, el) {
-
-        $('.block-catalog .conter').children('ul').append('<li><a>' + $(el).text() + '</a></li><li style="list-style-type:none"><ul></ul></li>');
-
-        var $tags = $(this).nextUntil('h3');
-
-        $tags.each(function(i, el) {
-            var $h4 = $(this)[0].tagName.toLowerCase();
-            if ($h4 === 'h4') {
-                $('.block-catalog').find('li[style] ul').eq(index).append('<li><a href="">' + $(this).text() + '</a></li>')
-                //console.log($(this), i, index)
-            };
-        });
-        //console.log('---------------');
-    });
-
-    $('.lotus-post').find('h3,h4').each(function(index, el) {
-        $(this).attr({
-            'class': 'anchor',
-            'id': 'anchor' + index
-        });
-    });
+    
 
     $('.block-catalog').find('a').each(function(index, el) {
         $(this).attr('href', '#anchor' + index);
