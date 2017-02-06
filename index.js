5a47c02e87e4ae7a857a0d1d6afa794ff1957037
longchan <longchan@mia.com>
2017-02-06 15:30:09 +0800

    新增webpack压缩样式脚本

---
+++ index.js
@@ -0,0 +1,8 @@
+//webpack entry
+
+require('./static/style/sass/style.scss');
+require('./static/style/pygments.css');
+
+require('./static/js/effect.js');
+require('./static/js/iLotus.js');
+require('./static/js/jquery.scrollTo.js');
